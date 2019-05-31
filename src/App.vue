0ca9b9af18309e99125b5163cacb12540d7dba8a
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2019-05-31 16:40:58 +0200

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ title}}</h1>
+    <p>{{ greeting()}}</p>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+        title: 'Your first Vue file, wooo!'
+    }
+  },
+  methods:{
+    greeting: function(){
+      return 'Hey cowboy!';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
