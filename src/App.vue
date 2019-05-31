92e5e0062c5060cf7e5c3fa484caf1607ce17f00
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2019-05-31 23:23:20 +0200

    Props ist use to transfer the data from the parent to its child. and the Child components can transfer the notification to parents components throght the events

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
       <app-header></app-header>
-      <app-ninjas></app-ninjas>
+      <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
       <app-footer></app-footer>
     </div>
 </template>
@@ -18,7 +18,14 @@ export default{
   },
   data(){
     return{
-
+      ninjas:[
+        {name:'Ryu', speciality:'Vue Components', show:false},
+        {name:'Crystal', speciality:'HTML Wizardry', show:false},
+        {name:'Hitoshi', speciality:'Chlick Events', show:false},
+        {name:'Tango', speciality:'Conditionals', show:false},
+        {name:'Kami', speciality:'Webpack', show:false},
+        {name:'Yoshi', speciality:'Data Diggin', show:false}
+      ],
     }
   }
 }
