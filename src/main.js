ded56f9cdb9d07f29ad1039eac3e15d35d983ff7
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2019-06-06 00:15:23 +0200

    This is how to create filter. Filter is the thing to express the data. It does not manipulate the data but change the output of the data. Example: uppercase, lowercase

--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
+//Filters
+Vue.filter('to-uppercase', function(value){
+  return value.toUpperCase();
+});
+
+Vue.filter('snippet', function(value){
+  return value.slice(0,100) + '...';
+})
+
 //Custom Directives
 Vue.directive('rainbow',{
   bind(el,binding, vnode){
