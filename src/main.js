361130059cc8a607ba5a6290b39e18e60f38bd0d
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2019-06-10 22:50:58 +0200

    This is how to register things locally. The previous lesson was create them globally

--- src/main.js
+++ src/main.js
@@ -6,20 +6,12 @@ import VueResource from 'vue-resource'
 Vue.use(VueResource);
 
 //Filters
-Vue.filter('to-uppercase', function(value){
-  return value.toUpperCase();
-});
-
 Vue.filter('snippet', function(value){
   return value.slice(0,100) + '...';
 });
 
 //Custom Directives
-Vue.directive('rainbow',{
-  bind(el,binding, vnode){
-    el.style.color = "#"+Math.random().toString().slice(2,8);
-  }
-});
+
 
 Vue.directive('theme',{
   bind(el, binding, vnode){
