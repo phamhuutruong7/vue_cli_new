4257a0252e589565734c6994ea54e55ee92b34dc
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2019-06-10 23:47:21 +0200

    This is how to make the link to the header of the page. Also make the exact

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,40 +1,40 @@
 <template>
-    <header>
-        <h1 v-on:click="changeTitle">{{title}}</h1>
-    </header>
+  <nav>
+    <ul>
+      <li><router-link to="/" exact>Blog</router-link></li>
+      <li><router-link to="/add" exact>Add a New Blog</router-link></li>
+    </ul>
+  </nav>
 </template>
 
 <script>
-import {bus} from '../main';
-
 export default {
-  props:{
-    title:{
-      type:String
-    }
-  },
-    data () {
-        return {
-          title: 'Vue Ninjas'
-        }
-    },
-    methods:{
-      changeTitle: function(){
-        //this.$emit('changeTitle','Vue Wizards');
-        this.title = 'Vue Wizards';
-        bus.$emit('titleChanged','Vue Wizards');
-      }
-    }
 }
 </script>
 
 <style scoped>
-  header{
-    background: lightgreen;
-    padding: 10px;
-  }
-  h1{
-    color: #222;
-    text-align:center;
-  }
+ul{
+  list-style-type: none;
+  text-align: center;
+  margin: 0;
+}
+li{
+  display: inline-block;
+  margin: 0 10px;
+}
+a{
+  color: #fff;
+  text-decoration: none;
+  padding: 6px 8px;
+  border-radius: 10px;
+}
+nav{
+  background: #444;
+  padding: 14px 0;
+  margin-bottom: 40px;
+}
+.router-link-active{
+  background: #eee;
+  color: #444;
+}
 </style>
